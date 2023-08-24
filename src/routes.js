7ccf64e88ca3c378417766b3d2466e55ddab03f6
natalie <nataliereneemcmullen@gmail.com>
2023-08-24 14:48:33 -0700

    little fix

--- src/routes.js
+++ src/routes.js
@@ -76,13 +76,13 @@ const routes = [
     component: Profile,
   },*/}
   ,
-  // {
-  //   name: "Sign In",
-  //   layout: "/auth",
-  //   path: "/sign-in",
-  //   icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
-  //   component: SignInCentered,
-  // },
+  {
+    name: "Sign In",
+    layout: "/auth",
+    path: "/sign-in",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: SignInCentered,
+  },
   {/*
   {
     name: "RTL Admin",
